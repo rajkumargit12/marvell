77ffc24132e5c23c6c978e86928e86388c4dcacc
rajkumargit12 <113007231+rajkumargit12@users.noreply.github.com>
2022-09-11 09:04:24 +0530

    updation

--- output.tf
+++ output.tf
@@ -4,17 +4,17 @@ output "region" {
   value = data.aws_region.current
 
 }
-#"arn:aws:logs:ap-southeast-1:234949410202:log-group:/aws/lambda/demoupload:*"
-# output "aws_vpc" {
-#   value = aws_vpc.vpc.id
-# }
-# output "aws_key_pair" {
-#   value = aws_key_pair.master.id
-# }
-# output "aws_ami" {
-#   value = aws_instance.apache1.id
-}
+# #"arn:aws:logs:ap-southeast-1:234949410202:log-group:/aws/lambda/demoupload:*"
+# # output "aws_vpc" {
+# #   value = aws_vpc.vpc.id
 # # }
-# output "aws_instance"{
-#   value = aws_instance.public_ip.id
+# # output "aws_key_pair" {
+# #   value = aws_key_pair.master.id
+# # }
+# # output "aws_ami" {
+# #   value = aws_instance.apache1.id
 # }
+# # # }
+# # output "aws_instance"{
+# #   value = aws_instance.public_ip.id
+# # }
